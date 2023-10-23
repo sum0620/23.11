04b6733f738d2cfe1ed0bc9dae911db3bcedcef0
USER <sumeyrah.2006.soydan@gmail.com>
2023-10-23 13:26:41 +0300

    uyg23.11

---
+++ app/src/main/java/alfabe.java
@@ -0,0 +1,12 @@
+public class alfabe {
+    public static void main(String[] args) {
+        int ascii;
+        for(ascii=65;ascii<=89;ascii++){
+            char kr= (char)ascii;
+            System.out.println(kr);
+            System.out.println(ascii);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/ascii.java
@@ -0,0 +1,11 @@
+public class ascii {
+    public static void main(String[] args) {
+        int ascii;
+
+        for (ascii = 122; ascii >96; ascii--) {
+            char karakter = (char) ascii;
+            System.out.println("Karakter" +" "+ karakter);
+
+        }
+    }
+}
